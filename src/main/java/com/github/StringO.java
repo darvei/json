ec34f0d6b1aba1f1e796af9ba841a1ebf5f5a193
Darvei <darvei@163.com>
2018-08-17 17:56:50 +0800

    not used at all
    removed

--- src/main/java/com/github/StringO.java
+++ src/main/java/com/github/StringO.java
@@ -9,7 +9,7 @@ import java.util.regex.PatternSyntaxException;
  * @author qiaozhy
  *
  */
-public class StringO extends ObjectO {
+public class StringO {
 
 	public static final String EMPTY = "";
 	
@@ -19,31 +19,14 @@ public class StringO extends ObjectO {
 	public static final String SPACE = "\b";
 	public static final String PAGE = "\f";
 
-	public StringO(Object object) {
-		super(object);
-	}
-
 	public static boolean isNull(String string) {
-		if (NULL == string) return true;
+		if (null == string) return true;
 		return false;
-	}
-
-	public String getString() {
-		if (isNull()) return EMPTY;
-
-		Object obj = this.get();
-		if (obj instanceof String) return (String) obj;
-		else return EMPTY;
 	}
 
 	public static String trim(String string) {
 		if (!isNull(string)) return string.trim();
 		return EMPTY;
-	}
-
-	public String trim() {
-		if (isNull()) return EMPTY;
-		return this.getString().trim();
 	}
 
 	/**
@@ -56,19 +39,9 @@ public class StringO extends ObjectO {
 		if (!isNull(string) && string.length() == 0) return true;
 		return false;
 	}
-	
-	public boolean isEmpty() {
-		if(!isNull() && this.getString().length() == 0) return true;
-		return false;
-	}
 
 	public static boolean isNotEmpty(String string) {
 		if (!isNull(string) && string.length() != 0) return true;
-		return false;
-	}
-	
-	public boolean isNotEmpty() {
-		if (!isNull() && this.getString().length() != 0) return true;
 		return false;
 	}
 
@@ -82,19 +55,9 @@ public class StringO extends ObjectO {
 		if (!isNull(string) && string.length() != 0 && string.trim().length() == 0) return true;
 		return false;
 	}
-	
-	public boolean isWhites() {
-		if (!isNull() && this.getString().length() != 0 && this.getString().trim().length() == 0) return true;
-		return false;
-	}
 
 	public static boolean isNotWhites(String string) {
 		if (isNull(string) || string.length() == 0 || string.trim().length() != 0) return true;
-		return false;
-	}
-	
-	public boolean isNotWhites() {
-		if (isNull() || this.getString().length() == 0 || this.getString().trim().length() != 0) return true;
 		return false;
 	}
 
@@ -109,11 +72,6 @@ public class StringO extends ObjectO {
 		if (isNull(string) || string.length() == 0 || string.trim().length() == 0) return true;
 		return false;
 	}
-	
-	public boolean isBlank() {
-		if (isNull() || this.getString().length() == 0 || this.getString().trim().length() == 0) return true;
-		return false;
-	}
 
 	/**
 	 * Sometimes means useful.
@@ -123,11 +81,6 @@ public class StringO extends ObjectO {
 	 */
 	public static boolean isNotBlank(String string) {
 		if (!isNull(string) && string.length() != 0 && string.trim().length() != 0) return true;
-		return false;
-	}
-	
-	public boolean isNotBlank() {
-		if (!isNull() || this.getString().length() != 0 || this.getString().trim().length() != 0) return true;
 		return false;
 	}
 	
@@ -165,11 +118,6 @@ public class StringO extends ObjectO {
 		return EMPTY;
 	}
 	
-	public String subString(String prefix, String suffix) {
-		String string = this.getString();
-		return subString(prefix, suffix, string);
-	}
-	
 	/**
 	 * String between a start and/or an end tag. Eg:
 	 * 1) [[abc] between [[] is abc.
@@ -203,65 +151,6 @@ public class StringO extends ObjectO {
 			s = s.substring(0, pos);
 		}
 		return s;
-	}
-	
-	public String between(String prefix, String suffix) {
-		String string = this.getString();
-		return between(prefix, suffix, string);
-	}
-	
-	/**
-	 * The first after part.
-	 * @param begin
-	 * @param string
-	 * @return
-	 */
-	/*public static String firstIndexAfter(String begin, String string) {
-		if (isNotEmpty(string) && isNotEmpty(begin)) {
-			int pos = string.indexOf(begin);
-			if (pos > 0) {
-				pos += begin.length();
-				return string.substring(pos, string.length());
-			}
-		}
-		return string;
-	}*/
-	
-	/**
-	 * The last before part.
-	 */
-	public static String lastIndexBefore(String tag, String string) {
-		if (isNotEmpty(string) && isNotEmpty(tag)) {
-			int pos = string.lastIndexOf(tag);
-			if (pos < string.length() && pos >= 0) {
-				return string.substring(0, pos);
-			}
-		}
-		return string;
-	}
-	
-	/**
-	 * The last after part.
-	 */
-	public static String lastIndexAfter(String tag, String string) {
-		if (isNotEmpty(string) && isNotEmpty(tag)) {
-			int pos = string.lastIndexOf(tag);
-			if (pos < string.length() && pos >= 0) {
-				pos ++;
-				return string.substring(pos, string.length());
-			}
-		}
-		return string;
-	}
-	
-	public static String[] split(String string, String exp) {
-		if (StringO.isBlank(string)) return new String[0];
-		
-		try {
-			return string.split(exp);
-		} catch (PatternSyntaxException e) {
-			return new String[0];
-		}
 	}
 	
 	/**
